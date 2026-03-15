d186869715508159e8c1741b74c454533366e815
jai <nireshpandian19@gmail.com>
2026-03-15 22:00:44 +0530

    Toggl env

--- config/config.py
+++ config/config.py
@@ -88,7 +88,7 @@ DEBUG_ENABLED = os.getenv("DEBUG", "false").lower() == "true"
 RETRY_THRESHOLD = int(os.getenv("RETRY_THRESHOLD", "3"))
 
 # When true, health check is forced to fail so generate requests always trigger session-close retry
-FORCE_HEALTH_CHECK_FAIL = os.getenv("FORCE_HEALTH_CHECK_FAIL", "false").lower() == "true"
+FORCE_HEALTH_CHECK_FAIL = os.getenv("FORCE_HEALTH_CHECK_FAIL", "true").lower() == "true"
 
 SHOULD_RESTART_ON_FAILURE = os.getenv("SHOULD_RESTART_ON_FAILURE", "false").lower() == "true"
 
